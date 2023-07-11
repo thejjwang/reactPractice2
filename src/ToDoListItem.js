c24b57bc063d75fea4c37841faa889d0d305ad4f
thejjwang <jonathanjacobwang@gmail.com>
2023-07-10 17:34:21 -0700

    trying to refactor into component

--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const ToDoListItem = (props) => {
-    <li >
-    {props.todos}<button onClick={props.deleteToDo}>Delete</button>
-    </li>
+    return (
+        <li>
+            {props.todos}<button onClick={props.deleteToDo}>Delete</button>
+        </li>
+    )
 }
 
 export default ToDoListItem;
